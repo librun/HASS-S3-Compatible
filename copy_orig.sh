f75d70f14eb4a58497ed88ee9fc4222f43592a24
Ilya Mazaev <limp2006@ya.ru>
2025-05-15 18:51:02 +0300

    add script copy from origin repo

---
+++ copy_orig.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+CWD=$(pwd)
+TMP_DIR="${CWD}/tmp"
+ORIG_PATH="/homeassistant/components/aws_s3"
+NEW_DIR="${CWD}/custom_components/s3_compatible"
+
+clean_up() {
+    rm -rf ./tmp/
+}
+
+clone_from_orig() {
+    git clone -b master -n --depth=1 --filter=tree:0 https://github.com/home-assistant/core.git ${TMP_DIR}
+    cd tmp
+    git sparse-checkout set --no-cone ${ORIG_PATH}
+    git checkout
+    cd $CWD
+}
+
+mv_from_orig() {
+    mv ${TMP_DIR}${ORIG_PATH}/__init__.py ${NEW_DIR}/__init__.py
+    mv ${TMP_DIR}${ORIG_PATH}/backup.py ${NEW_DIR}/backup.py
+    mv ${TMP_DIR}${ORIG_PATH}/const.py ${NEW_DIR}/const.py
+    mv ${TMP_DIR}${ORIG_PATH}/config_flow.py ${NEW_DIR}/config_flow.py
+    mv ${TMP_DIR}${ORIG_PATH}/strings.json ${NEW_DIR}/strings.json
+}
+
+clean_up
+clone_from_orig
+mv_from_orig
+clean_up
